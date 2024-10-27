f8d2a504462ae56c9e1fb4a27c39c46e3d12f509
brunasso <53584556+brunasso@users.noreply.github.com>
2024-10-27 20:32:59 +0000

    Finalizando modificaciones

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,9 +1,82 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
-	let [listaDeTareas, setListaDeTareas] = useState(["bañarse", "limpiar", "cocinar", "aprender react"]);
+	let [listaDeTareas, setListaDeTareas] = useState([]);
 	const [nuevaTarea, setNuevaTarea] = useState("");
+	const user = 'brunasso';
+	const urlUser = `https://playground.4geeks.com/todo/users/${user}`;
+	const urlTodo = 'https://playground.4geeks.com/todo/todos/'
+
+
+	const addNewTask = async(evento) => {
+		if (evento.key === "Enter") {
+			const resp = await fetch(`${urlTodo}${user}`, {
+				method: "POST",
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					label: nuevaTarea,
+					is_done: false
+				})
+			});
+			if(resp.ok){
+				renderTasks();
+			}
+		}
+	}
+
+	const createUser = async() => {
+		try {
+			const userAdd = await fetch(urlUser, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			})
+			if(userAdd.status == '200') renderTasks()
+		} catch (error) {
+			console.error("Error al intentar agregar usuario")
+		}
+	}
+
+	const renderTasks = async() => {
+		try {
+			const datos = await fetch(urlUser)
+			if (datos.status == '404') {
+				createUser();
+				return null
+			}
+
+			const informacion = await datos.json();
+
+			setListaDeTareas(informacion.todos)
+
+		} catch (error) {
+			console.error("Hubo un error al cargar tareas");
+			setListaDeTareas([]);
+		}
+
+	}
+
+	const deleteTask = async(index) =>{
+		try {
+			const deletedTask = await fetch(`${urlTodo}${index}`, {
+				method: 'DELETE',			
+			})		
+			if (deletedTask.ok) {
+				renderTasks();
+			}
+		} catch (error) {
+			console.error("Error al eliminar tarea")
+		}
+	}
+
+	useEffect(()=>{
+		renderTasks();
+	},[])
+
 	return (
 		<div className="container mt-5">
 			<h1 className="text-center mt-5">Todos</h1>
@@ -16,20 +89,20 @@ const Home = () => {
 						value={nuevaTarea}
 						onChange={(evento) => setNuevaTarea(evento.target.value)}
 						onKeyUp={(evento) => {
-							if (evento.key === "Enter") {
-								setListaDeTareas([...listaDeTareas, nuevaTarea]);
-								setNuevaTarea("");
-							}
+							addNewTask(evento);
 						}}
 					/>
 				</div>
 				<div className="list-group">
-					{listaDeTareas.map((tarea, index) => (
-						<div className="list-group-item d-flex justify-content-between align-items-center" key={index}>
-							{tarea}
+					{listaDeTareas.map(task => (
+						<div className="list-group-item d-flex justify-content-between align-items-center" key={task.id}>
+							{task.label}
 							<i onClick={() => {
-								const aux = listaDeTareas.filter((_task, ind) => ind !== index);
-								setListaDeTareas(aux);
+
+								deleteTask(task.id)
+
+								/* const aux = listaDeTareas.filter((_task, ind) => ind !== index);
+								setListaDeTareas(aux); */
 							}} className="fa-solid fa-trash icono-oculto"></i>
 						</div>
 					))}
